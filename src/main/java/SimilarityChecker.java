b234168cbf71d95ed236d6f28ae046bfed255df7
ingyuseo <sig45@naver.com>
2025-06-04 16:29:50 +0900

    [feature] 글자수검사 구현

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,7 +1,21 @@
 public class SimilarityChecker {
 
-    public int getLengthSimilarity(String str1, String str2) {
-        if (str1.length() == str2.length()) return 60;
-        return 0;
+    public double getLengthSimilarity(String str1, String str2) {
+        int longer_len;
+        int shorter_len;
+
+        if(str1.length() > str2.length()){
+            longer_len = str1.length();
+            shorter_len = str2.length();
+        }
+        else{
+            longer_len = str2.length();
+            shorter_len = str1.length();
+        }
+
+        int gap = longer_len - shorter_len;
+        double ratio = (double) gap /shorter_len;
+        if(ratio > 1 ) ratio = 1;
+        return (1-ratio)*60;
     }
 }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -1,13 +1,30 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityCheckerTest {
+    private SimilarityChecker similarityChecker;
+
+    @BeforeEach
+    void setUp() {
+        similarityChecker = new SimilarityChecker();
+    }
+
     @Test
     void returnIfSameLength(){
-        SimilarityChecker similarityChecker = new SimilarityChecker();
-        int ret = similarityChecker.getLengthSimilarity("AA","BB");
-
-        assertEquals(ret, 60);
+        assertEquals(60, similarityChecker.getLengthSimilarity("AA","BB"));
     }
+
+    @Test
+    void returnIfDiffLengthLess2(){
+        assertEquals(30, similarityChecker.getLengthSimilarity("AAAAAA","BBBB"));
+    }
+
+    @Test
+    void returnIfDiffLengthOver2(){
+        assertEquals(0,similarityChecker.getLengthSimilarity("AAAAAA","BB"));
+    }
+
+
 }
