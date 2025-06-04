36c37c0cce8b3e58b1958426c043500eb4ec4c24
ingyuseo <sig45@naver.com>
2025-06-04 16:11:34 +0900

    [feature] 같은 길이

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,2 +1,7 @@
 public class SimilarityChecker {
+
+    public int getLengthSimilarity(String str1, String str2) {
+        if (str1.length() == str2.length()) return 60;
+        return 0;
+    }
 }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -1,5 +1,13 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityCheckerTest {
+    @Test
+    void returnIfSameLength(){
+        SimilarityChecker similarityChecker = new SimilarityChecker();
+        int ret = similarityChecker.getLengthSimilarity("AA","BB");
 
+        assertEquals(ret, 60);
+    }
 }
