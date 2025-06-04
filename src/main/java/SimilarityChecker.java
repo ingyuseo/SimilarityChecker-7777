8b0cd8dc2a1e653abe0aed39751a5db3a34f9883
ingyuseo <sig45@naver.com>
2025-06-04 16:33:56 +0900

    [refactor] 글자수검사 구현

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,21 +1,19 @@
 public class SimilarityChecker {
 
     public double getLengthSimilarity(String str1, String str2) {
-        int longer_len;
-        int shorter_len;
+        int len1 = str1.length();
+        int len2 = str2.length();
 
-        if(str1.length() > str2.length()){
-            longer_len = str1.length();
-            shorter_len = str2.length();
-        }
-        else{
-            longer_len = str2.length();
-            shorter_len = str1.length();
-        }
+        int longer = Math.max(len1, len2);
+        int shorter = Math.min(len1, len2);
 
-        int gap = longer_len - shorter_len;
-        double ratio = (double) gap /shorter_len;
-        if(ratio > 1 ) ratio = 1;
-        return (1-ratio)*60;
+        double ratio = (double) (longer - shorter) / shorter;
+        return getResult(ratio);
+    }
+
+    private double getResult(double ratio) {
+        double result = (1 - ratio) * 60;
+        if(result < 0) result =0;
+        return result;
     }
 }
